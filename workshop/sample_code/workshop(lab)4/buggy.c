e9b3087edbe97aa27a7c4ebab8e4ed84c6bde9cf
Harry <RedAndBlueEraser@outlook.com>
2016-03-21 02:29:17 +0000

    added workshop c programs from week 4

---
+++ workshop/sample_code/workshop(lab)4/buggy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct{
+	int value;
+}data_t;
+
+int
+main(int argc, char *argv[]){
+    int x = 7;
+    printf("x= %d\n" , x;
+	data_t *d = NULL;
+	printf("value = %d\n" ,d->value);
+	return 0;	
+}
+
---
+++ workshop/sample_code/workshop(lab)4/gen_input.py
@@ -0,0 +1,4 @@
+bytes = [0xFF, 0xFF, 0xFF]
+overflow_str = ('a'*some_number + ''.join(something_here))
+print overflow_str
+print 'ownz_u!'
